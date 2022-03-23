ea35f21f980d06fc950dc5d47da9998ae809379b
lipliepra <dzis99@yandex.ru>
2022-03-23 20:15:17 +0500

    Fix

--- Redux/src/App.tsx
+++ Redux/src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import Header from "./components/Header";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import FilterGroup from "./components/FilterGroup";
+import DeleteTodos from "./components/DeleteTodos";
+
+import { tabs } from "./helpers/tabs";
+
 const App: React.FC = () => {
-  return <div className="App">fdvdfvdfvdfv</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <FilterGroup tabs={tabs} currentType={"all"} setType={() => {}} sort={true} setSort={() => {}} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
